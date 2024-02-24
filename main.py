8a4c872b4654eba8ebe5b169198dec96d082d2da
Marcel Grego <marcelgrego@wp.pl>
2024-02-24 20:55:28 +0100

    Third commit

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ from convertor import *
 def run_program() -> None:
     file_path = input("Enter file path: ")
     print("1) csv -> json\n2) json -> csv")
-    option_num = int(input("Option nr: "))
-    if option_num == 1:
-        csv2json(file_path)
-    elif option_num == 2:
-        json2csv(file_path)
-    else:
+
+    try:
+        option_num = int(input("Option nr: "))
+    except ValueError:
         print("There is no such option!")
+    else:
+        if option_num == 1:
+            csv2json(file_path)
+        elif option_num == 2:
+            json2csv(file_path)
+        else:
+            print("There is no such option!")
+    finally:
+        print("Closing...")
 
 
 if __name__ == "__main__":
